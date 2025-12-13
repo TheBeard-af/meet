be07f6e949ee3571eab4be6a5aae5c2455affc6b
TheBeard-af <afaqnadeem1993@gmail.com>
2025-12-13 13:18:39 +1100

    Added Atatus integration for performance monitoring

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import * as atatus from "atatus-spa";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+atatus.config("93a5259ce4244b73bf28b599c2c52f55").install();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
