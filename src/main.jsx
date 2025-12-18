3f23beb617d5c715b2e5d47d3ec839c306382a1d
TheBeard-af <afaqnadeem1993@gmail.com>
2025-12-18 21:00:30 +1100

    bash git status git add . git commit -m "Enable PWA service worker and manifest

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import * as atatus from "atatus-spa";
+// import * as atatus from "atatus-spa";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
-atatus.config("93a5259ce4244b73bf28b599c2c52f55").install();
+// atatus.config("93a5259ce4244b73bf28b599c2c52f55").install();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+serviceWorkerRegistration.register();
