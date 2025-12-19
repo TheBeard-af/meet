1ec4e8fb7db9897d473918539a2df23c84a5142d
TheBeard-af <afaqnadeem1993@gmail.com>
2025-12-19 16:45:01 +1100

    Implement data visualizations with Recharts
    Add scatterplot

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { useEffect, useState } from "react";
 import { extractLocations, getEvents } from "./api";
 import "./App.css";
 import { InfoAlert, ErrorAlert, WarningAlert } from "./components/Alert";
+import CityEventsChart from "./components/CityEventsChart";
 
 const App = () => {
   const [events, setEvents] = useState([]);
@@ -40,6 +41,7 @@ const App = () => {
 
   return (
     <div className="App">
+      <h1>Meet App</h1>
       <div className="alerts-container">
         {infoAlert.length ? <InfoAlert text={infoAlert} /> : null}
         {errorAlert.length ? <ErrorAlert text={errorAlert} /> : null}
@@ -55,6 +57,7 @@ const App = () => {
         setCurrentNOE={setCurrentNOE}
         setErrorAlert={setErrorAlert}
       />
+      <CityEventsChart allLocations={allLocations} events={events} />
       <EventList events={events} />
     </div>
   );
