28427dcc33e175dfa28e663b0f93ea09de540ff6
TheBeard-af <afaqnadeem1993@gmail.com>
2025-12-20 14:29:33 +1100

    Add event genres pie chart and responsive charts layout

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import { extractLocations, getEvents } from "./api";
 import "./App.css";
 import { InfoAlert, ErrorAlert, WarningAlert } from "./components/Alert";
 import CityEventsChart from "./components/CityEventsChart";
+import EventGenresChart from "./components/EventGenresChart";
 
 const App = () => {
   const [events, setEvents] = useState([]);
@@ -61,7 +62,10 @@ const App = () => {
         setCurrentNOE={setCurrentNOE}
         setErrorAlert={setErrorAlert}
       />
-      <CityEventsChart allLocations={allLocations} events={events} />
+      <div className="charts-container">
+        <EventGenresChart events={events} />
+        <CityEventsChart allLocations={allLocations} events={events} />
+      </div>
       <EventList events={events} />
     </div>
   );
